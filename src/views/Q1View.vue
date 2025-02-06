1a5b6386f76147e80a56f4daf48d6fca4dc3751f
gabaddotey <gabaddotey@gmail.com>
2025-02-06 14:52:53 +0000

    question option list WORKS

--- src/views/Q1View.vue
+++ src/views/Q1View.vue
@@ -1,15 +1,7 @@
 <script setup lang="ts">
 import MultiQuestion from '../components/MultiSelect.vue'
-import {ref} from 'vue'
 
-const questionList = ref([
-  { id: 1, questionTitle: 'My journey with Vue' },
-  { id: 2, questionTitle: 'Blogging with Vue' },
-  { id: 3, questionTitle: 'Why Vue is so fun' }
-])
-
-const getOpList = () => [1, 2, 3, 4, 5]
-const oplist = ref(getOpList())
+const oplist = ['qwerty','246','abc']
 
 </script>
 
@@ -19,7 +11,7 @@ const oplist = ref(getOpList())
     <h1>Q1</h1>
   
     <!--<MultiQuestion props.questiontitle="How are you?" :question-options="['good', 'bad']" next-question="/q2"/>-->
-    <MultiQuestion question-options= abc next-question="???" id=1></MultiQuestion>
+    <MultiQuestion id=1 :question-options = oplist next-question="???" ></MultiQuestion>
   </div>
 
 </template>
