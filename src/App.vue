0c3ddab2cb8931c7f942babcaaea37858b284b32
gabaddotey <gabaddotey@gmail.com>
2025-04-14 11:53:31 +0000

    vercel analytics

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import NavBar from '@/components/NavBar.vue';
+import { inject } from '@vercel/analytics';
 </script>
 
 <template>
