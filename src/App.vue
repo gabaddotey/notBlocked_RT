8447f2c4a16af3cb158d0afbceaaaec035109d5d
gabaddotey <gabaddotey@gmail.com>
2025-04-14 13:45:43 +0000

    analytics

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import NavBar from '@/components/NavBar.vue';
-import { inject } from '@vercel/analytics';
+import Analytics from '@vercel/analytics';
 </script>
 
 <template>
+  <Analytics />
   <!-- OG template <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
